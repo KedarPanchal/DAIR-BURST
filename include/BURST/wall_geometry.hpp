6eff963bb9e0543d72268dbb269a1b4f7455dd91
KedarPanchal <kedar.t.panchal@outlook.com>
2026-02-10 12:04:47 -0600

    Finish Configuration Geometry implementation code (almost) and update rendering

--- include/BURST/wall_geometry.hpp
+++ include/BURST/wall_geometry.hpp
@@ -5,6 +5,10 @@
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Polygon_2.h>
+#include <CGAL/Aff_transformation_2.h>
+#include <CGAL/Graphics_scene.h>
+#include <CGAL/draw_polygon_2.h>
+#include <CGAL/centroid.h>
 
 #include "renderable.hpp"
 #include "configuration_geometry.hpp"
@@ -14,6 +18,7 @@ using Kernel = CGAL::Exact_predicates_exact_constructions_kernel;
 using Point_2 = Kernel::Point_2;
 using Segment_2 = Kernel::Segment_2;
 using Polygon_2 = CGAL::Polygon_2<Kernel>;
+using scene = CGAL::Graphics_scene;
 
 namespace BURST::geometry {
     
@@ -27,17 +32,32 @@ namespace BURST::geometry {
         public:
             ConfigurationGeometryImpl(std::initializer_list<Point_2> edge_endpoints) : configuration_shape{Polygon_2(edge_endpoints.begin(), edge_endpoints.end())} {}
 
-            std::optional<Segment_2> getEdge(Point_2 intersection_point) const override;
-            std::optional<Segment_2> getEdge(Segment_2 intersection_segment) const noexcept override {
+            std::optional<Segment_2> getEdge(Point_2 intersection_point) const override {
                 for (auto edge_it = this->configuration_shape.edges_begin(); edge_it != this->configuration_shape.edges_end(); edge_it++) {
-                    if (edge_it->has_on(intersection_segment.source()) && edge_it->has_on(intersection_segment.target())) {
+                    // TODO: For now, the first edge containing the point is returned
+                    // This doesn't handle corners
+                    // The paper doesn't handle corners, either, so let's deflect blame and procrastinate
+                    if (edge_it->has_on(intersection_point)) {
                         return std::optional<Segment_2>{*edge_it};
                     }
                 }
                 return std::nullopt;
             }
+            std::optional<Segment_2> getEdge(Segment_2 intersection_segment) const noexcept override {
+                for (auto edge_it = this->configuration_shape.edges_begin(); edge_it != this->configuration_shape.edges_end(); edge_it++) {
+                    // Check if both endpoints of the intersection segment are on the edge
+                    // This means that the intersection is a subsegment of the edge
+                    if (edge_it->has_on(intersection_segment.source()) && edge_it->has_on(intersection_segment.target())) {
+                        return std::optional<Segment_2>{*edge_it};
+                    }
+                }
+                // No edge :(
+                return std::nullopt;
+            }
 
-            void render() const override;
+            void render(scene& scene) const override {
+                CGAL::add_to_graphics_scene(this->configuration_shape, scene); 
+            }
         };
     }
 
@@ -53,7 +73,9 @@ namespace BURST::geometry {
         void generateConfigurationGeometry(Robot& robot) const {
         }
 
-        void render() const override;
+        void render(scene& scene) const override {
+            CGAL::add_to_graphics_scene(this->wall_shape, scene);
+        }
     };
 
 }
