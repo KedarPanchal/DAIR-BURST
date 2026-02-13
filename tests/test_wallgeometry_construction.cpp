dfde7b021dc0109b87832e555f8c287c05ed2e9d
KedarPanchal <kedar.t.panchal@outlook.com>
2026-02-12 20:14:16 -0600

    Rename WallGeometry test names

--- tests/test_wallgeometry_construction.cpp
+++ tests/test_wallgeometry_construction.cpp
@@ -5,7 +5,7 @@
 #include <optional>
 
 // Test for intended non-degeneracy with a regular polygon
-TEST(WallGeometryTest, NonDegenerateRegularPolygon) {
+TEST(WallGeometryConstructionTest, NonDegenerateRegularPolygon) {
     // Construct a WallGeometry for a square
     auto wall_geometry = BURST::geometry::WallGeometry::create({
         Point_2(0, 0),
@@ -20,7 +20,7 @@ TEST(WallGeometryTest, NonDegenerateRegularPolygon) {
 }
 
 // Test for intended non-degeneracy with a simple polygon
-TEST(WallGeometryTest, NonDegenerateSimplePolygon) {
+TEST(WallGeometryConstructionTest, NonDegenerateSimplePolygon) {
     // Construct a WallGeometry for a simple polygon
     // In this case, we'll use a concave polygon with an arrowhead shape
     auto wall_geometry = BURST::geometry::WallGeometry::create({
@@ -36,7 +36,7 @@ TEST(WallGeometryTest, NonDegenerateSimplePolygon) {
 }
 
 // Test for intended degeneracy with a straight line
-TEST(WallGeometryTest, DegenerateStraightLine) {
+TEST(WallGeometryConstructionTest, DegenerateStraightLine) {
     // Construct a WallGeometry for a straight line
     // This is a degenerate polygon
     auto wall_geometry = BURST::geometry::WallGeometry::create({
@@ -51,7 +51,7 @@ TEST(WallGeometryTest, DegenerateStraightLine) {
 }
 
 // Test for intended degeneracy with a single point
-TEST(WallGeometryTest, DegenerateSinglePoint) {
+TEST(WallGeometryConstructionTest, DegenerateSinglePoint) {
     // Construct a WallGeometry for a single point
     // This is a degenerate polygon
     auto wall_geometry = BURST::geometry::WallGeometry::create({
@@ -64,7 +64,7 @@ TEST(WallGeometryTest, DegenerateSinglePoint) {
 }
 
 // Test for intended degeneracy with a repeated point
-TEST(WallGeometryTest, DegenerateRepeatedPoint) {
+TEST(WallGeometryConstructionTest, DegenerateRepeatedPoint) {
     // Construct a WallGeometry for a repeated point
     // This is a degenerate polygon
     auto wall_geometry = BURST::geometry::WallGeometry::create({
@@ -81,7 +81,7 @@ TEST(WallGeometryTest, DegenerateRepeatedPoint) {
 
 // Test for intended degeneracy with a self-intersecting polygon
 // While this isn't truly degenerate, it is still invalid for our purposes and should be rejected
-TEST(WallGeometryTest, DegenerateSelfIntersectingPolygon) {
+TEST(WallGeometryConstructionTest, DegenerateSelfIntersectingPolygon) {
     // Construct a WallGeometry for a self-intersecting polygon
     // This polygon looks like an hourglass and intersects itself at the center
     // This is a degenerate polygon for our purposes
@@ -98,7 +98,7 @@ TEST(WallGeometryTest, DegenerateSelfIntersectingPolygon) {
 }
 
 // Test for intended degeneracy with a a normally valid polygon with out-of-order points
-TEST(WallGeometryTest, DegenerateOutOfOrderPoints) {
+TEST(WallGeometryConstructionTest, DegenerateOutOfOrderPoints) {
     // Construct a WallGeometry for a normally valid polygon with out-of-order points
     // This polygon is a square but the points are given in an order that creates a bowtie shape
     // This is a degenerate polygon for our purposes
