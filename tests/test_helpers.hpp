676c3379e001067765bfc31e30750f229a34aa1f
KedarPanchal <kedar.t.panchal@outlook.com>
2026-02-17 22:34:53 -0600

    Abstract MovementModel test fixtures to test_helpers header

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -1,6 +1,7 @@
 #ifndef TEST_WALLGEOMETRY_CLASS_HPP
 #define TEST_WALLGEOMETRY_CLASS_HPP
 
+#include <gtest/gtest.h>
 #include <BURST/wall_geometry.hpp>
 #include <BURST/configuration_geometry.hpp>
 #include <BURST/types.hpp>
@@ -26,6 +27,82 @@ public:
     }
     std::unique_ptr<BURST::geometry::ConfigurationGeometry> testConstructConfigurationGeometry(BURST::fscalar robot_radius) const {
         return this->constructConfigurationGeometry(robot_radius);
+    }
+};
+
+// Define a test fixture containing a valid ConfigurationGeometry for testing the MovementModel
+// This should be reused across multiple tests for the MovementModel to keep the geometry consistent
+// This test fixture uses a square configuration geometry
+class MovementModelInSquareTest : public ::testing::Test {
+protected:
+    std::unique_ptr<BURST::geometry::ConfigurationGeometry> configuration_geometry;
+    BURST::Point_2 corner_vertex;
+    BURST::Point_2 edge_midpoint;
+
+    void SetUp() override {
+        // Construct a TestWallGeometry for a square and generate a ConfigurationGeometry with a robot radius of 1
+        auto wall_geometry = TestWallGeometry::create({
+            BURST::Point_2{0, 0},
+            BURST::Point_2{10, 0},
+            BURST::Point_2{10, 10},
+            BURST::Point_2{0, 10}
+        });
+        // Expect the WallGeometry to be non-degenerate
+        // i.e. it is not nullopt
+        ASSERT_TRUE(wall_geometry.has_value()) << "Failed to construct non-degenerate WallGeometry for a regular polygon in test fixture setup";
+
+        // Construct a configuration geometry for a robot with radius 1
+        this->configuration_geometry = wall_geometry->testConstructConfigurationGeometry(1.0);
+        ASSERT_NE(this->configuration_geometry, nullptr) << "Failed to construct ConfigurationGeometry from WallGeometry in test fixture setup";
+
+        // Define a corner and midpoint for use in tests
+        this->corner_vertex = BURST::Point_2{1, 1};
+        this->edge_midpoint = BURST::Point_2{5, 1};
+    }
+};
+
+// Define test fixture containing a valid ConfigurationGeometry for testing the MovementModel
+// This should be reused across multiple tests for the MovementModel to keep the geometry consistent
+// This test fixture uses a concave configuration geometry
+// In this case, an arrowhead shape
+class MovementModelInConcaveTest : public ::testing::Test {
+protected:
+    std::unique_ptr<BURST::geometry::ConfigurationGeometry> configuration_geometry;
+    BURST::Point_2 concave_vertex;
+    BURST::Point_2 edge_midpoint;
+    
+    void SetUp() override {
+        // Construct a TestWallGeometry for a concave polygon and generate a ConfigurationGeometry with a robot radius of 1
+        auto wall_geometry = TestWallGeometry::create({
+            BURST::Point_2{0, 20},
+            BURST::Point_2{-20, -20},
+            BURST::Point_2{0, 0},
+            BURST::Point_2{20, -20}
+        });
+        // Expect the WallGeometry to be non-degenerate
+        // i.e. it is not nullopt
+        ASSERT_TRUE(wall_geometry.has_value()) << "Failed to construct non-degenerate WallGeometry for a simple polygon in test fixture setup";
+
+        // Construct a configuration geometry for a robot with radius 1
+        this->configuration_geometry = wall_geometry->testConstructConfigurationGeometry(1.0);
+        ASSERT_NE(this->configuration_geometry, nullptr) << "Failed to construct ConfigurationGeometry from WallGeometry in test fixture setup";
+        
+
+        // Identify the concave vertex of the configuration geometry for use in tests
+        for (auto vertex_it = this->configuration_geometry->vertex_begin(); vertex_it != this->configuration_geometry->vertex_end(); vertex_it++) {
+            if (vertex_it->x() == 0 && vertex_it->y() < 2) {
+                this->concave_vertex = *vertex_it;
+                break;
+            }
+        }
+        
+        // Find an edge of the configuration geometry that contains the concave vertex and identify its midpoint for use in tests
+        for (auto edge_it = this->configuration_geometry->edge_begin(); edge_it != this->configuration_geometry->edge_end(); edge_it++) {
+            if (edge_it->target().x() == 0 && edge_it->target().y() < 2) {
+                this->edge_midpoint = CGAL::midpoint(edge_it->source(), edge_it->target());
+                break;
+            }
+        }
     }
 };
 
--- tests/test_movementmodel_functor.cpp
+++ tests/test_movementmodel_functor.cpp
@@ -5,85 +5,9 @@
 #include "test_helpers.hpp"
 #include <optional>
 
-// Define a test fixture containing a valid ConfigurationGeometry for testing the MovementModel
-// This should be reused across multiple tests for the MovementModel to keep the geometry consistent
-// This test fixture uses a square configuration geometry
-class MovementModelInSquareTest : public ::testing::Test {
-protected:
-    std::unique_ptr<BURST::geometry::ConfigurationGeometry> configuration_geometry;
-    BURST::Point_2 corner_vertex;
-    BURST::Point_2 edge_midpoint;
-
-    void SetUp() override {
-        // Construct a TestWallGeometry for a square and generate a ConfigurationGeometry with a robot radius of 1
-        auto wall_geometry = TestWallGeometry::create({
-            BURST::Point_2{0, 0},
-            BURST::Point_2{10, 0},
-            BURST::Point_2{10, 10},
-            BURST::Point_2{0, 10}
-        });
-        // Expect the WallGeometry to be non-degenerate
-        // i.e. it is not nullopt
-        ASSERT_TRUE(wall_geometry.has_value()) << "Failed to construct non-degenerate WallGeometry for a regular polygon in test fixture setup";
-
-        // Construct a configuration geometry for a robot with radius 1
-        this->configuration_geometry = wall_geometry->testConstructConfigurationGeometry(1.0);
-        ASSERT_NE(this->configuration_geometry, nullptr) << "Failed to construct ConfigurationGeometry from WallGeometry in test fixture setup";
-
-        // Define a corner and midpoint for use in tests
-        this->corner_vertex = BURST::Point_2{1, 1};
-        this->edge_midpoint = BURST::Point_2{5, 1};
-    }
-};
-
-// Define test fixture containing a valid ConfigurationGeometry for testing the MovementModel
-// This should be reused across multiple tests for the MovementModel to keep the geometry consistent
-// This test fixture uses a concave configuration geometry
-// In this case, an arrowhead shape
-class MovementModelInConcaveTest : public ::testing::Test {
-protected:
-    std::unique_ptr<BURST::geometry::ConfigurationGeometry> configuration_geometry;
-    BURST::Point_2 concave_vertex;
-    BURST::Point_2 edge_midpoint;
-    
-    void SetUp() override {
-        // Construct a TestWallGeometry for a concave polygon and generate a ConfigurationGeometry with a robot radius of 1
-        auto wall_geometry = TestWallGeometry::create({
-            BURST::Point_2{0, 20},
-            BURST::Point_2{-20, -20},
-            BURST::Point_2{0, 0},
-            BURST::Point_2{20, -20}
-        });
-        // Expect the WallGeometry to be non-degenerate
-        // i.e. it is not nullopt
-        ASSERT_TRUE(wall_geometry.has_value()) << "Failed to construct non-degenerate WallGeometry for a simple polygon in test fixture setup";
-
-        // Construct a configuration geometry for a robot with radius 1
-        this->configuration_geometry = wall_geometry->testConstructConfigurationGeometry(1.0);
-        ASSERT_NE(this->configuration_geometry, nullptr) << "Failed to construct ConfigurationGeometry from WallGeometry in test fixture setup";
-        
-
-        // Identify the concave vertex of the configuration geometry for use in tests
-        for (auto vertex_it = this->configuration_geometry->vertex_begin(); vertex_it != this->configuration_geometry->vertex_end(); vertex_it++) {
-            if (vertex_it->x() == 0 && vertex_it->y() < 2) {
-                this->concave_vertex = *vertex_it;
-                break;
-            }
-        }
-        
-        // Find an edge of the configuration geometry that contains the concave vertex and identify its midpoint for use in tests
-        for (auto edge_it = this->configuration_geometry->edge_begin(); edge_it != this->configuration_geometry->edge_end(); edge_it++) {
-            if (edge_it->target().x() == 0 && edge_it->target().y() < 2) {
-                this->edge_midpoint = CGAL::midpoint(edge_it->source(), edge_it->target());
-                break;
-            }
-        }
-    }
-};
-
 // Test generating a valid movement with a linear movement model
 // This means that the movement should be in a straight line towards the interior of the configuration geometry
- TEST_F(MovementModelInSquareTest, ValidLinearMovementInSquare) {
+TEST_F(MovementModelInSquareTest, ValidLinearMovementInSquare) {
     // Construct a LinearMovementModel
     auto movement_model = BURST::models::LinearMovementModel{};
     // Use the midpoint of the bottom edge of the configuration geometry as the origin
@@ -97,7 +21,7 @@ protected:
 }
 
 // Test generating a valid movement with a linear movement model at a corner
- TEST_F(MovementModelInSquareTest, ValidLinearMovementAtCornerInSquare) {
+TEST_F(MovementModelInSquareTest, ValidLinearMovementAtCornerInSquare) {
     // Construct a LinearMovementModel
     auto movement_model = BURST::models::LinearMovementModel{};
     // Use the corner vertex of the configuration geometry as the origin
@@ -129,7 +53,7 @@ TEST_F(MovementModelInSquareTest, ValidLinearMovementAlongEdgeInSquare) {
 }
 
 // Test generating an invalid movement with a linear movement model with a point inside but not on the configuration geometry edge
- TEST_F(MovementModelInSquareTest, InvalidInteriorLinearMovementInSquare) {
+TEST_F(MovementModelInSquareTest, InvalidInteriorLinearMovementInSquare) {
     // Construct a LinearMovementModel
     auto movement_model = BURST::models::LinearMovementModel{};
     // Specify an origin at the center of the configuration geometry
@@ -143,7 +67,7 @@ TEST_F(MovementModelInSquareTest, ValidLinearMovementAlongEdgeInSquare) {
 }
 
 // Test generating an invalid movement with a linear movement model with a point outside the configuration geometry
- TEST_F(MovementModelInSquareTest, InvalidExteriorLinearMovementInSquare) {
+TEST_F(MovementModelInSquareTest, InvalidExteriorLinearMovementInSquare) {
     // Construct a LinearMovementModel
     auto movement_model = BURST::models::LinearMovementModel{};
     // Specify an origin outside the configuration geometry
@@ -157,7 +81,7 @@ TEST_F(MovementModelInSquareTest, ValidLinearMovementAlongEdgeInSquare) {
 }
 
 // Test generating an invalid movement with a linear movement model with a point on the edge but a direction pointing outside the configuration geometry
- TEST_F(MovementModelInSquareTest, InvalidLinearMovementPointingOutwardInSquare) {
+TEST_F(MovementModelInSquareTest, InvalidLinearMovementPointingOutwardInSquare) {
     // Construct a LinearMovementModel
     auto movement_model = BURST::models::LinearMovementModel{};
     // Use the midpoint of the bottom edge of the configuration geometry as the origin
