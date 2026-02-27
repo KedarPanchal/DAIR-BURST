e08dcf0aef8d94fec19c01fc054c823a73b46f05
KedarPanchal <kedar.t.panchal@outlook.com>
2026-02-26 23:13:05 -0600

    Add test file

---
+++ tests/test_configurationspace_intersections.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <BURST/geometric_types.hpp>
+#include <BURST/configuration_space.hpp>
+#include <BURST/wall_space.hpp>
+
+#include "test_helpers.hpp"
+
+// Utility includes for tests
