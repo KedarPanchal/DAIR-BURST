a291b407e4a61033df11db34edfb0eebda69a6c8
KedarPanchal <kedar.t.panchal@outlook.com>
2026-02-27 09:12:21 -0600

    Add tests for Configurationspace::intersection on a square room

--- tests/test_configurationspace_intersections.cpp
+++ tests/test_configurationspace_intersections.cpp
@@ -94,7 +94,7 @@ TEST(ConfigurationSpaceIntersectionTest, InvalidPointIntersectionRegularPolygon)
     EXPECT_FALSE(configuration_space->intersection(intersection_point).has_value()) << "Expected point to not intersect with the ConfigurationSpace, but got a valid intersection";
 }
 
-// Test ray intersection for a ConfigurationSpace with a regular polygon
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts on the edge of the ConfigurationSpace and points inward
 TEST(ConfigurationSpaceIntersectionTest, RayIntersectionRegularPolygon) {
     // Construct an already known non-degenerate WallSpace for a square
     auto wall_space = TestWallSpace::create({
@@ -119,9 +119,201 @@ TEST(ConfigurationSpaceIntersectionTest, RayIntersectionRegularPolygon) {
     std::vector<BURST::geometry::Point2D> intersections;
 
     // Compute the intersections of the ray with the ConfigurationSpace
-    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Point2D>(ray, std::back_inserter(intersections));
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
 
     // Expect the ray to intersect with the ConfigurationSpace exactly once
     // i.e., ConfigurationSpace::intersection == 1
     EXPECT_EQ(intersection_count, 1) << "Expected ray to intersect with the ConfigurationSpace exactly once, but got " << intersection_count << " intersections";
 }  
+
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts at the corner of the ConfigurationSpace and points inward along the angle bisector of the corner
+TEST(ConfigurationSpaceIntersectionTest, RatIntersectionAtCornerRegularPolygon) {
+    // Construct an already known non-degenerate WallSpace for a square
+    auto wall_space = TestWallSpace::create({
+        BURST::geometry::Point2D{0, 0},
+        BURST::geometry::Point2D{10, 0},
+        BURST::geometry::Point2D{10, 10},
+        BURST::geometry::Point2D{0, 10}
+    });
+    // Expect the WallSpace to be non-degenerate
+    // i.e. it is not nullopt
+    ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+    // Construct a ConfigurationSpace for a robot with radius 1
+    auto configuration_space = wall_space->testConstructConfigurationSpace(1);
+    // Expect the ConfigurationSpace to be non-degenerate
+    // i.e. it is not nullptr
+    ASSERT_NE(configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+
+    // Create a ray that starts at the corner of the ConfigurationSpace and points inward along the angle bisector of the corner
+    BURST::geometry::Ray2D ray{BURST::geometry::Point2D{1, 1}, BURST::geometry::Vector2D{1, 1}};
+    // Create a collection for the intersections
+    std::vector<BURST::geometry::Point2D> intersections;
+
+    // Compute the intersections of the ray with the ConfigurationSpace
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
+
+    // Expect the ray to intersect with the ConfigurationSpace exactly twice, since it should intersect with both edges that meet at the corner endpoint
+    // i.e., ConfigurationSpace::intersection == 2
+    EXPECT_EQ(intersection_count, 2) << "Expected ray to intersect with the ConfigurationSpace exactly once, but got " << intersection_count << " intersections";
+}
+
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts interior to the ConfigurationSpace
+TEST(ConfigurationSpaceIntersectionTest, RayIntersectionInteriorRegularPolygon) {
+    // Construct an already known non-degenerate WallSpace for a square
+    auto wall_space = TestWallSpace::create({
+        BURST::geometry::Point2D{0, 0},
+        BURST::geometry::Point2D{10, 0},
+        BURST::geometry::Point2D{10, 10},
+        BURST::geometry::Point2D{0, 10}
+    });
+    // Expect the WallSpace to be non-degenerate
+    // i.e. it is not nullopt
+    ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+    // Construct a ConfigurationSpace for a robot with radius 1
+    auto configuration_space = wall_space->testConstructConfigurationSpace(1);
+    // Expect the ConfigurationSpace to be non-degenerate
+    // i.e. it is not nullptr
+    ASSERT_NE(configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+
+    // Create a ray that starts at the interior of the ConfigurationSpace and points to an edge
+    BURST::geometry::Ray2D ray{BURST::geometry::Point2D{5, 5}, BURST::geometry::Vector2D{1, 0}};
+    // Create a collection for the intersections
+    std::vector<BURST::geometry::Point2D> intersections;
+
+    // Compute the intersections of the ray with the ConfigurationSpace
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
+
+    // Expect the ray to intersect with the ConfigurationSpace exactly once, since it should only intersect with the edge that the ray points towards
+    // i.e., ConfigurationSpace::intersection == 1
+    EXPECT_EQ(intersection_count, 1) << "Expected ray to intersect with the ConfigurationSpace exactly once, but got " << intersection_count << " intersections";
+}
+
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts exterior to the ConfigurationSpace
+TEST(ConfigurationSpaceIntersectionTest, RayIntersectionExteriorRegularPolygon) {
+    // Construct an already known non-degenerate WallSpace for a square
+    auto wall_space = TestWallSpace::create({
+        BURST::geometry::Point2D{0, 0},
+        BURST::geometry::Point2D{10, 0},
+        BURST::geometry::Point2D{10, 10},
+        BURST::geometry::Point2D{0, 10}
+    });
+    // Expect the WallSpace to be non-degenerate
+    // i.e. it is not nullopt
+    ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+    // Construct a ConfigurationSpace for a robot with radius 1
+    auto configuration_space = wall_space->testConstructConfigurationSpace(1);
+    // Expect the ConfigurationSpace to be non-degenerate
+    // i.e. it is not nullptr
+    ASSERT_NE(configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+
+    // Create a ray that starts at the exterior of the ConfigurationSpace and points towards the ConfigurationSpace
+    BURST::geometry::Ray2D ray{BURST::geometry::Point2D{15, 5}, BURST::geometry::Vector2D{-1, 0}};
+    // Create a collection for the intersections
+    std::vector<BURST::geometry::Point2D> intersections;
+
+    // Compute the intersections of the ray with the ConfigurationSpace
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
+
+    // Expect the ray to intersect with the ConfigurationSpace exactly twice, since it should intersect with both the top and bottom edges of the ConfigurationSpace
+    // i.e., ConfigurationSpace::intersection == 2
+    EXPECT_EQ(intersection_count, 2) << "Expected ray to intersect with the ConfigurationSpace exactly twice, but got " << intersection_count << " intersections";
+}
+
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts on the edge of the ConfigurationSpace and points outward
+TEST(ConfigurationSpaceIntersectionTest, RayIntersectionOutwardRegularPolygon) {
+    // Construct an already known non-degenerate WallSpace for a square
+    auto wall_space = TestWallSpace::create({
+        BURST::geometry::Point2D{0, 0},
+        BURST::geometry::Point2D{10, 0},
+        BURST::geometry::Point2D{10, 10},
+        BURST::geometry::Point2D{0, 10}
+    });
+    // Expect the WallSpace to be non-degenerate
+    // i.e. it is not nullopt
+    ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+    // Construct a ConfigurationSpace for a robot with radius 1
+    auto configuration_space = wall_space->testConstructConfigurationSpace(1);
+    // Expect the ConfigurationSpace to be non-degenerate
+    // i.e. it is not nullptr
+    ASSERT_NE(configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+
+    // Create a ray that starts at the edge of the ConfigurationSpace and points outside the ConfigurationSpace
+    BURST::geometry::Ray2D ray{BURST::geometry::Point2D{1, 5}, BURST::geometry::Vector2D{-1, 0}};
+    // Create a collection for the intersections
+    std::vector<BURST::geometry::Point2D> intersections;
+
+    // Compute the intersections of the ray with the ConfigurationSpace
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
+
+    // Expect the ray to not intersect with the ConfigurationSpace
+    // i.e., ConfigurationSpace::intersection == 0
+    EXPECT_EQ(intersection_count, 0) << "Expected ray to not intersect with the ConfigurationSpace, but got " << intersection_count << " intersections";
+}
+
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts on the corner of the ConfigurationSpace and points outward
+TEST(ConfigurationSpaceIntersectionTest, RayIntersectionOutwardAtCornerRegularPolygon) {
+    // Construct an already known non-degenerate WallSpace for a square
+    auto wall_space = TestWallSpace::create({
+        BURST::geometry::Point2D{0, 0},
+        BURST::geometry::Point2D{10, 0},
+        BURST::geometry::Point2D{10, 10},
+        BURST::geometry::Point2D{0, 10}
+    });
+    // Expect the WallSpace to be non-degenerate
+    // i.e. it is not nullopt
+    ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+    // Construct a ConfigurationSpace for a robot with radius 1
+    auto configuration_space = wall_space->testConstructConfigurationSpace(1);
+    // Expect the ConfigurationSpace to be non-degenerate
+    // i.e. it is not nullptr
+    ASSERT_NE(configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+
+    // Create a ray that starts at the corner of the ConfigurationSpace and points outside the ConfigurationSpace
+    BURST::geometry::Ray2D ray{BURST::geometry::Point2D{1, 1}, BURST::geometry::Vector2D{-1, -1}};
+    // Create a collection for the intersections
+    std::vector<BURST::geometry::Point2D> intersections;
+
+    // Compute the intersections of the ray with the ConfigurationSpace
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
+
+    // Expect the ray to not intersect with the ConfigurationSpace no times
+    // i.e., ConfigurationSpace::intersection == 0
+    EXPECT_EQ(intersection_count, 0) << "Expected ray to not intersect with the ConfigurationSpace, but got " << intersection_count << " intersections";
+}
+
+// Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts at the exterior of the ConfigurationSpace and points outward
+TEST(ConfigurationSpaceIntersectionTest, RayIntersectionOutwardExteriorRegularPolygon) {
+    // Construct an already known non-degenerate WallSpace for a square
+    auto wall_space = TestWallSpace::create({
+        BURST::geometry::Point2D{0, 0},
+        BURST::geometry::Point2D{10, 0},
+        BURST::geometry::Point2D{10, 10},
+        BURST::geometry::Point2D{0, 10}
+    });
+    // Expect the WallSpace to be non-degenerate
+    // i.e. it is not nullopt
+    ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+    // Construct a ConfigurationSpace for a robot with radius 1
+    auto configuration_space = wall_space->testConstructConfigurationSpace(1);
+    // Expect the ConfigurationSpace to be non-degenerate
+    // i.e. it is not nullptr
+    ASSERT_NE(configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+
+    // Create a ray that starts at the exterior of the ConfigurationSpace and points outside the ConfigurationSpace
+    BURST::geometry::Ray2D ray{BURST::geometry::Point2D{15, 5}, BURST::geometry::Vector2D{1, 0}};
+    // Create a collection for the intersections
+    std::vector<BURST::geometry::Point2D> intersections;
+
+    // Compute the intersections of the ray with the ConfigurationSpace
+    size_t intersection_count = configuration_space->intersection<BURST::geometry::Ray2D, BURST::geometry::Segment2D>(ray, std::back_inserter(intersections));
+
+    // Expect the ray to not intersect with the ConfigurationSpace no times
+    // i.e., ConfigurationSpace::intersection == 0
+    EXPECT_EQ(intersection_count, 0) << "Expected ray to not intersect with the ConfigurationSpace, but got " << intersection_count << " intersections";
+}
