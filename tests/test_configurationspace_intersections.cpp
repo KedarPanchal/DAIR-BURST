52f8aecde4f887d22071d1ea123ece5c694d4775
KedarPanchal <kedar.t.panchal@outlook.com>
2026-03-02 22:26:54 -0600

    Make ConfigurationSpace::intersection tests more readable

--- tests/test_configurationspace_intersections.cpp
+++ tests/test_configurationspace_intersections.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 #include <iterator>
 
-// -- REGULAR POLYGON INTERSECTION TESTS ---------------------------------------
+// -- TEST FIXTURE SETUP -------------------------------------------------------
 
 // Create a test fixture for ConfigurationSpace intersection tests with a regular polygon
 class ConfigurationSpaceRegularPolygonIntersectionTest : public ::testing::Test {
@@ -37,6 +37,35 @@ protected:
         ASSERT_NE(this->configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
     }
 };
+
+// Create a test fixture for ConfigurationSpace intersection tests with a simple, but concave polygon
+class ConfigurationSpaceConcavePolygonIntersectionTest : public ::testing::Test {
+protected:
+    std::unique_ptr<BURST::geometry::ConfigurationSpace> configuration_space;
+
+    void SetUp() override {
+        auto wall_space = TestWallSpace::create({
+            // Construct an already known non-degenerate WallSpace for a simple polygon
+            // In this case, we'll use a concave polygon with an arrowhead shape
+            BURST::geometry::Point2D{0, 20},
+            BURST::geometry::Point2D{-20, -20},
+            BURST::geometry::Point2D{0, 0},
+            BURST::geometry::Point2D{20, -20}
+        });
+        // Expect the WallSpace to be non-degenerate
+        // i.e. it is not nullopt
+        ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
+
+        // Construct a ConfigurationSpace for a robot with radius 1
+        this->configuration_space = wall_space->testConstructConfigurationSpace(1);
+        // Expect the ConfigurationSpace to be non-degenerate
+        // i.e. it is not nullptr
+        ASSERT_NE(this->configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
+    }
+};
+
+
+// -- REGULAR POLYGON POINT INTERSECTION TESTS ---------------------------------
 
 // Test point intersection for a ConfigurationSpace with a regular polygon
 // TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
@@ -81,6 +110,45 @@ TEST_F(ConfigurationSpaceRegularPolygonIntersectionTest, InvalidPointIntersectio
     // i.e., ConfigurationSpace::intersection is nullopt
     EXPECT_FALSE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to not intersect with the ConfigurationSpace, but got a valid intersection";
 }
+
+
+// -- CONCAVE POLYGON POINT INTERSECTION TESTS ---------------------------------
+
+// Test point intersection for a ConfigurationSpace with a concave polygon
+// TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
+TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, PointIntersectionConcavePolygon) {
+    // Create a point that lies on the edge of the ConfigurationSpace
+    BURST::geometry::Point2D intersection_point{0, 1};
+
+    // Expect the point to intersect with the ConfigurationSpace
+    // i.e., ConfigurationSpace::intersection is not nullopt
+    EXPECT_TRUE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to intersect with the ConfigurationSpace, but got nullopt";
+}
+
+// Test invalid point intersection on the interior of a ConfigurationSpace with a concave polygon
+// TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
+TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, InvalidPointIntersectionConcavePolygon) {
+    // Create a point that lies not on the boundary of the ConfigurationSpace
+    BURST::geometry::Point2D intersection_point{0, 5};
+
+    // Expect the point to not intersect with the ConfigurationSpace
+    // i.e., ConfigurationSpace::intersection is nullopt
+    EXPECT_FALSE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to not intersect with the ConfigurationSpace, but got a valid intersection";
+}
+
+// Test invalid point intersection on the exterior of a ConfigurationSpace with a concave polygon
+// TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
+TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, InvalidPointIntersectionExteriorConcavePolygon) {
+    // Create a point that lies not on the boundary of the ConfigurationSpace
+    BURST::geometry::Point2D intersection_point{0, 100};
+
+    // Expect the point to not intersect with the ConfigurationSpace
+    // i.e., ConfigurationSpace::intersection is nullopt
+    EXPECT_FALSE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to not intersect with the ConfigurationSpace, but got a valid intersection";
+}
+
+
+// -- REGULAR POLYGON RAY INTERSECTION TESTS -----------------------------------
 
 // Test ray intersection for a ConfigurationSpace with a regular polygon with a ray that starts on the edge of the ConfigurationSpace and points inward
 TEST_F(ConfigurationSpaceRegularPolygonIntersectionTest, RayIntersectionRegularPolygon) {
@@ -188,66 +256,7 @@ TEST_F(ConfigurationSpaceRegularPolygonIntersectionTest, RayIntersectionOutwardE
 }
 
 
-// -- CONCAVE POLYGON INTERSECTION TESTS ---------------------------------------
-
-// Create a test fixture for ConfigurationSpace intersection tests with a simple, but concave polygon
-class ConfigurationSpaceConcavePolygonIntersectionTest : public ::testing::Test {
-protected:
-    std::unique_ptr<BURST::geometry::ConfigurationSpace> configuration_space;
-
-    void SetUp() override {
-        auto wall_space = TestWallSpace::create({
-            // Construct an already known non-degenerate WallSpace for a simple polygon
-            // In this case, we'll use a concave polygon with an arrowhead shape
-            BURST::geometry::Point2D{0, 20},
-            BURST::geometry::Point2D{-20, -20},
-            BURST::geometry::Point2D{0, 0},
-            BURST::geometry::Point2D{20, -20}
-        });
-        // Expect the WallSpace to be non-degenerate
-        // i.e. it is not nullopt
-        ASSERT_TRUE(wall_space.has_value()) << "Failed to construct non-degenerate WallSpace";
-
-        // Construct a ConfigurationSpace for a robot with radius 1
-        this->configuration_space = wall_space->testConstructConfigurationSpace(1);
-        // Expect the ConfigurationSpace to be non-degenerate
-        // i.e. it is not nullptr
-        ASSERT_NE(this->configuration_space, nullptr) << "Failed to construct non-degenerate ConfigurationSpace";
-    }
-};
-
-// Test point intersection for a ConfigurationSpace with a concave polygon
-// TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
-TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, PointIntersectionConcavePolygon) {
-    // Create a point that lies on the edge of the ConfigurationSpace
-    BURST::geometry::Point2D intersection_point{0, 1};
-
-    // Expect the point to intersect with the ConfigurationSpace
-    // i.e., ConfigurationSpace::intersection is not nullopt
-    EXPECT_TRUE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to intersect with the ConfigurationSpace, but got nullopt";
-}
-
-// Test invalid point intersection on the interior of a ConfigurationSpace with a concave polygon
-// TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
-TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, InvalidPointIntersectionConcavePolygon) {
-    // Create a point that lies not on the boundary of the ConfigurationSpace
-    BURST::geometry::Point2D intersection_point{0, 5};
-
-    // Expect the point to not intersect with the ConfigurationSpace
-    // i.e., ConfigurationSpace::intersection is nullopt
-    EXPECT_FALSE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to not intersect with the ConfigurationSpace, but got a valid intersection";
-}
-
-// Test invalid point intersection on the exterior of a ConfigurationSpace with a concave polygon
-// TODO: Once the point intersection function is updated to return an edge, update this test case accordingly
-TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, InvalidPointIntersectionExteriorConcavePolygon) {
-    // Create a point that lies not on the boundary of the ConfigurationSpace
-    BURST::geometry::Point2D intersection_point{0, 100};
-
-    // Expect the point to not intersect with the ConfigurationSpace
-    // i.e., ConfigurationSpace::intersection is nullopt
-    EXPECT_FALSE(this->configuration_space->intersection(intersection_point).has_value()) << "Expected point to not intersect with the ConfigurationSpace, but got a valid intersection";
-}
+// -- CONCAVE POLYGON RAY INTERSECTION TESTS -----------------------------------
 
 // Test ray intersection for a ConfigurationSpace with a concave polygon with a ray that starts on the edge of the ConfigurationSpace and points inward
 TEST_F(ConfigurationSpaceConcavePolygonIntersectionTest, RayIntersectionConcavePolygon) {
