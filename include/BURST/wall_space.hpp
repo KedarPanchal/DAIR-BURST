03b0d0bcf455ce325f80b2c0ca6ae2a16aca21e7
KedarPanchal <kedar.t.panchal@outlook.com>
2026-03-03 21:26:32 -0600

    Reduce code duplication in WallSpace::create methods

--- include/BURST/wall_space.hpp
+++ include/BURST/wall_space.hpp
@@ -26,9 +26,8 @@ namespace BURST::geometry {
         template <typename C, typename V>
         concept valid_wall_space_input_collection = std::ranges::sized_range<C> && std::same_as<std::remove_cv_t<std::ranges::range_value_t<C>>, V>;
     }
-    /*
-     * WallSpace represents the geometry of the walls in the environment. It is defined by a polygon.
-     */
+
+    // WallSpace represents the geometry of the walls in the environment. It is defined by a polygon.
     class WallSpace : public Renderable {
     private:
         
@@ -36,17 +35,33 @@ namespace BURST::geometry {
         graphics::PolygonOptions wall_render_options;
 
     protected: 
-        /* 
-         * Protected constructors since the public API is through the static create method
-         * Abstracting this away to protected constructors allows subclassing WallSpace in a test environment without depending on the static create method and its constraints
-         */
+        // Protected constructors since the public API is through the static create method
+        // Abstracting this away to protected constructors allows subclassing WallSpace in a test environment without depending on the static create method and its constraints
         WallSpace(const Polygon2D& shape) noexcept : wall_shape{shape} {}
         WallSpace(const HoledPolygon2D& shape) noexcept : wall_shape{shape} {}
 
-        /*
-         * Protected method since the public API depends on the robot
-         * Abstracting this away to a protected method allows subclassing WallSpace in a test environment without depending on the Robot class
-         */
+        // Protected methods since the public API depends on the robot
+        // Abstracting this away to a protected method allows subclassing WallSpace in a test environment without depending on the Robot class
+
+        // Creates a polygon from a collection of points, ensuring the polygon is oriented counterclockwise and simple
+        template <detail::valid_wall_space_input_collection<Point2D> C>
+        static std::optional<Polygon2D> createPolygon(C points) {
+            // Can't make a polygon with 2 or fewer points
+            if (points.size() <= 2) return std::nullopt;
+            // Check for self-intersection and overall simplicity of the polygon
+            if (!CGAL::is_simple_2(points.begin(), points.end())) return std::nullopt;
+            // If there's collinear points, the polygon is degenerate, so we can't create a wall geometry
+            if (CGAL::orientation_2(points.begin(), points.end()) == CGAL::COLLINEAR) return std::nullopt;
+
+            // Create the wall polygon from the input points
+            Polygon2D polygon{points.begin(), points.end()};
+            // If the polygon is not oriented counterclockwise, reverse the orientation to ensure it's a valid polygon for CGAL
+            if (polygon.orientation() != CGAL::COUNTERCLOCKWISE) polygon.reverse_orientation();
+
+            return std::optional<Polygon2D>{polygon};
+        }
+
+        // Constructs a configuration space given a robot radius
         std::unique_ptr<ConfigurationSpace> constructConfigurationSpace(const numeric::fscalar& robot_radius) const {
             // TODO: Find an actually good epsilon instead of this approximation
             const double EPSILON = 0.000001;
@@ -54,11 +69,10 @@ namespace BURST::geometry {
             // Compute the inset of the outer boundary of the wall polygon
             std::vector<CurvilinearPolygon2D> outer_inset_results;
             CGAL::approximated_inset_2(this->wall_shape.outer_boundary(), robot_radius, EPSILON, std::back_inserter(outer_inset_results));
-            /*
-             * If there are no polygons, then the wall is too small for the robot and no configuration space could be made
-             * If there are multiple polygons, then there were regions too tight for the robot to fit in, and no configuration space could be made
-             * In both cases, return nullptr
-             */
+
+            // If there are no polygons, then the wall is too small for the robot and no configuration space could be made
+            // If there are multiple polygons, then there were regions too tight for the robot to fit in, and no configuration space could be made
+            // In both cases, return nullptr
             if (outer_inset_results.size() != 1) return nullptr;
             // Reverse the resulting polygon's rotation if it's not counterclockwise
             if (outer_inset_results.front().orientation() != CGAL::COUNTERCLOCKWISE) outer_inset_results.front().reverse_orientation();
@@ -68,20 +82,15 @@ namespace BURST::geometry {
             config_polygon_set.insert(outer_inset_results.front());
 
             // Compute the outset of all of the holes within the wall polygon, since the holes need to be expanded by the robot radius as well
+            // No checks needed for the holes touching the wall since that's a realistic case for when an object is close to a wall
             for (const auto& hole : this->wall_shape.holes()) {
                 // Holes are CW, so reverse the orientation to ensure it's a valid polygon for CGAL
                 Polygon2D oriented_hole = hole;
                 if (oriented_hole.orientation() != CGAL::COUNTERCLOCKWISE) oriented_hole.reverse_orientation();
-
-                /* 
-                 * No checks needed for the holes touching the wall since that's a realistic case for when an object is close to a wall
-                 * The polygon set should already be insulated against border touching, so we can safely ignore these checks
-                 */
-                
-                /*
-                 * Compute the difference between the resultant polygon set and outset hole
-                 * This is insulated against overlapping holes, which is possible if two holes are close enough that the space between is too small for the robot
-                 */
+ 
+                // Compute the difference between the resultant polygon set and outset hole
+                // This is insulated against overlapping holes and border touching
+                // This is possible if two holes or a hole and the wall are close enough that the space between is too small for the robot
                 config_polygon_set.difference(CGAL::approximated_offset_2(oriented_hole, robot_radius, EPSILON));
             }
             
@@ -92,31 +101,18 @@ namespace BURST::geometry {
     public:
         template <detail::valid_wall_space_input_collection<Point2D> C>
         static std::optional<WallSpace> create(C points) {
-            // Can't make a polygon with 2 or fewer points
-            if (points.size() <= 2) return std::nullopt;
-            // Check for self-intersection and overall simplicity of the polygon
-            if (!CGAL::is_simple_2(points.begin(), points.end())) return std::nullopt;
-            // If there's collinear points, the polygon is degenerate, so we can't create a wall geometry
-            if (CGAL::orientation_2(points.begin(), points.end()) == CGAL::COLLINEAR) return std::nullopt;
-
-            // Create the wall polygon from the input points
-            Polygon2D wall_polygon{points.begin(), points.end()};
-            // If the polygon is not oriented counterclockwise, reverse the orientation to ensure it's a valid polygon for CGAL
-            if (wall_polygon.orientation() != CGAL::COUNTERCLOCKWISE) wall_polygon.reverse_orientation();
-
-            return WallSpace{wall_polygon};
+            auto wall_polygon_opt = WallSpace::createPolygon(points);  
+            // If nullopt, then the wall polygon was degenerate and we can't create a wall geometry
+            return wall_polygon_opt.has_value() ? std::optional<WallSpace>{WallSpace{wall_polygon_opt.value()}} : std::nullopt;
         }
         template <detail::valid_wall_space_input_collection<Point2D> C1, detail::valid_wall_space_input_collection<Polygon2D> C2>
         static std::optional<WallSpace> create(C1 points, C2 holes) {
-            // Can't make a polygon with 2 or fewer points
-            if (points.size() <= 2) return std::nullopt;
-            // Create the wall polygon from the input points now for use in the hole validation checks
-            Polygon2D wall_polygon{points.begin(), points.end()};
-            // If the polygon is not oriented counterclockwise, reverse the orientation to ensure it's a valid polygon for CGAL
-            if (wall_polygon.orientation() != CGAL::COUNTERCLOCKWISE) wall_polygon.reverse_orientation();
+            auto wall_polygon_opt = WallSpace::createPolygon(points);
+            // Degenerate wall polygon, can't create a wall geometry
+            if (!wall_polygon_opt) return std::nullopt; 
 
             // Create a holed polygon to hold the holes and outer boundary
-            HoledPolygon2D wall_shape{wall_polygon};
+            HoledPolygon2D wall_shape{wall_polygon_opt.value()};
             
             // Check that each hole is clockwise-oriented
             for (const Polygon2D& hole : holes) {
@@ -127,12 +123,10 @@ namespace BURST::geometry {
                 wall_shape.add_hole(oriented_hole);
             }
 
-            /*
-             * Check if the resulting holed polygon is valid, which means:
-             * The outer boundary is a simple polygon and counterclockwise-oriented
-             * Each hole is a simple polygon and clockwise-oriented
-             * Holes are inside the outer boundary and do not intersect with each other
-             */
+            // Check if the resulting holed polygon is valid, which means:
+            // The outer boundary is a simple polygon and counterclockwise-oriented
+            // Each hole is a simple polygon and clockwise-oriented
+            // Holes are inside the outer boundary and do not intersect with each other
             if (!CGAL::is_valid_polygon_with_holes(wall_shape, LinearTraits{})) return std::nullopt;
 
             // If the resulting holed polygon is valid, construct the WallSpace
