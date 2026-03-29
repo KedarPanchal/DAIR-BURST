e29b2464b2fc86e7c704901b9afe2343116800dc
KedarPanchal <kedar.t.panchal@outlook.com>
2026-03-29 17:22:59 -0500

    Add error log macros

--- include/BURST/logging.hpp
+++ include/BURST/logging.hpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define BURST_WARNING_TO(msg, stream) do { \
-    ::fprintf(stream, "[BURST]\t%s\nFile:\t%s\nLine:\t%d\n", (msg), __FILE__, __LINE__); \
+    ::fprintf(stream, "[BURST] Warning\t%s\nFile:\t%s\nLine:\t%d\n", (msg), __FILE__, __LINE__); \
 } while(0)
 
 #define BURST_WARNING(msg) BURST_WARNING_TO(msg, stderr)
@@ -15,6 +15,23 @@
 // Define no-op macros if warnings are disabled to prevent compile errors
 #define BURST_WARNING_TO(msg, stream) do {} while(0)
 #define BURST_WARNING(msg) do {} while(0)
+
+#endif
+
+#ifndef BURST_DISABLE_ERRORS
+#include <stdio.h>
+
+#define BURST_ERROR_TO(msg, stream) do { \
+    ::fprintf(stream, "[BURST] Error\t%s\nFile:\t%s\nLine:\t%d\n", (msg), __FILE__, __LINE__); \
+} while(0)
+
+#define BURST_ERROR(msg) BURST_ERROR_TO(msg, stderr)
+
+#else
+
+// Define no-op macros if errors are disabled to prevent compile errors
+#define BURST_ERROR_TO(msg, stream) do {} while(0)
+#define BURST_ERROR(msg) do {} while(0)
 
 #endif
 
