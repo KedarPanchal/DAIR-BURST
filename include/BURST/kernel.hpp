3937d7dffa4a61bda1bdb4e7c7aa4c589097e2ab
KedarPanchal <kedar.t.panchal@outlook.com>
2026-04-09 11:36:36 -0500

    Move graphics types to Kernel

--- include/BURST/kernel.hpp
+++ include/BURST/kernel.hpp
@@ -4,6 +4,8 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
 #include <CGAL/Gps_segment_traits_2.h>
 #include <CGAL/Gps_circle_segment_traits_2.h>
+#include <CGAL/Graphics_scene.h>
+#include <CGAL/IO/Color.h>
 
 namespace BURST {
 
@@ -11,6 +13,10 @@ namespace BURST {
     using Kernel = CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt;
     using LinearTraits = CGAL::Gps_segment_traits_2<Kernel>;
     using CurvedTraits = CGAL::Gps_circle_segment_traits_2<Kernel>;
+
+    // Graphics types
+    using Scene = CGAL::Graphics_scene;
+    using Color = CGAL::IO::Color;
 }
 
 #endif
